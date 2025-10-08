f9524a40775c9c3ec0f0d55a1f534782e5d24f6b
franbreciano <francobreciano@gmail.com>
2025-10-08 08:59:23 -0400

    v1.0.6: Fix Hero component - remove unused TrendingUp import
    - Remove unused TrendingUp import from Hero component
    - Fix TypeScript compilation error for Docker build
    - Update version to 1.0.6

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { TrendingUp, Star } from 'lucide-react'
+import { Star } from 'lucide-react'
 import Button from './shared/BaseButton'
 import { COPY } from '../constants/copy'
 
